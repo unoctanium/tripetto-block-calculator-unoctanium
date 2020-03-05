b992e93009629602212f832dbabc14756c2c29a9
unoctanium <odo@maletzki.net>
2020-03-05 18:35:21 +0000

    updated colleector ro resolve slot values

--- src/collector/calculator.ts
+++ src/collector/calculator.ts
@@ -9,26 +9,39 @@ import {
     tripetto
 } from "tripetto-collector";
 import { ICalculator } from "../interface";
+import { castToFloat } from "tripetto-collector";
 @tripetto({
     type: "headless",
     identifier: PACKAGE_NAME
 })
 export class Calculator extends HeadlessBlock<ICalculator> {
     /** Contains the number slot with the value. */
-    //readonly calculatorSlot = assert(this.valueOf<number, Slots.Numeric>("calculator"));
     readonly calculatorSlot = assert(this.valueOf<number, Slots.Numeric>("calculator"));
 
     // get the arguments as numbers
-    a1 = parseFloat(this.props.argument1 || "");
-    a2 = parseFloat(this.props.argument2 || "");
-    a3 = parseFloat(this.props.argument3 || "");
-
-    
+    //a1 = parseFloat(this.props.argument1 || "");
+    //a2 = parseFloat(this.props.argument2 || "");
+    //a3 = parseFloat(this.props.argument3 || "");
 
     // assign the result of the functionType with args to the result slot
     do(): void {      
         var result = 0.0;
-        console.log(this.props.argument1);
+
+        const a1 = castToFloat(
+          this.props.argument1, 
+          castToFloat(this.variableFor(this.props.argument1)?.value)
+        ); 
+        const a2 = castToFloat(
+          this.props.argument2, 
+          castToFloat(this.variableFor(this.props.argument2)?.value)
+        ); 
+        const a3 = castToFloat(
+          this.props.argument3, 
+          castToFloat(this.variableFor(this.props.argument3)?.value)
+        );
+
+        //console.log(this.props.argument1);
+        
         switch (this.props.functionType) {
             case "add":
                 result = this.a1 + this.a2; 
